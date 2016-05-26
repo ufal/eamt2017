3e080cbf6a7bf5e54ad69c1a825b73e53132e9e5
Jan Ptacek <jan.ptacek@gmail.com>
2016-05-26 12:54:32 +0200

    initial

--- webhook.php
+++ webhook.php
@@ -1,4 +1,9 @@
 <?php
-	if ( $_POST['payload'] ) {
+	
+	if (@$_POST['payload'] ) {
 		  shell_exec( 'cd /usr/share/drupal7/legacy/eamt2017/ && git reset --hard HEAD && git pull' );
 	}
+	$res = array(
+		'res'=>'ok';
+	);
+	print_r($res);
