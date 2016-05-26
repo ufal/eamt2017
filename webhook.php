ba26efc7764646f10b1419c784b1e2016d27d1a7
Jan Ptacek <jan.ptacek@gmail.com>
2016-05-26 12:55:50 +0200

    typo

--- webhook.php
+++ webhook.php
@@ -1,9 +1,8 @@
 <?php
-	
 	if (@$_POST['payload'] ) {
 		  shell_exec( 'cd /usr/share/drupal7/legacy/eamt2017/ && git reset --hard HEAD && git pull' );
 	}
 	$res = array(
-		'res'=>'ok';
+		'res'=>'ok'
 	);
 	print_r($res);
