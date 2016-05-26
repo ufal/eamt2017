69f4d2fd6c146b680dbebac89fca1b5c890e77d1
Jan Ptacek <p@email.cz>
2016-05-26 13:04:25 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -4,7 +4,7 @@
 	);
 
 	if (@$_POST['payload'] ) {
-		$out = shell_exec( 'cd /usr/share/drupal7/legacy/eamt2017/ && git reset --hard HEAD && git pull' );
+		$out = shell_exec( 'cd /usr/share/drupal7/legacy/eamt2017/ && git reset --hard HEAD && git pull 2>&1' );
 		$res = array(
 			'res'=>'ok',
 			'out'=>$out
