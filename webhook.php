550f10b86db5c5be2453e62db6d8870e79330479
Jan Ptacek <jan.ptacek@gmail.com>
2016-05-26 13:56:20 +0200

    just on production

--- webhook.php
+++ webhook.php
@@ -3,7 +3,7 @@
 		'res'=>'nok'
 	);
 
-	if (@$_POST['payload'] ) {
+	if (@$_POST['payload'] && $_POST['payload']['ref'] == 'refs/heads/production') {
 		$out = shell_exec( 'cd /usr/share/drupal7/legacy/eamt2017/ && git reset --hard HEAD && git pull origin production 2>&1' );
 		$res = array(
 			'res'=>'ok',
