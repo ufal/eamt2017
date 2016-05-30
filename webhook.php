b44b535101dda61dad591dde829ea70ded823200
www-data <www-data@ufal.mff.cuni.cz>
2016-05-30 08:16:12 +0200

    testing for base_ref

--- webhook.php
+++ webhook.php
@@ -5,7 +5,7 @@
 
 	if (@$_POST['payload']) {
 		$pay = json_decode($_POST['payload']);
-		if ($pay->ref == 'refs/heads/production') {
+		if ($pay->base_ref == 'refs/heads/production') {
 			$out = shell_exec( 'cd /usr/share/drupal7/legacy/eamt2017/ && git reset --hard HEAD && git pull origin production 2>&1' );
 			$res = array(
 				'res'=>'ok',
