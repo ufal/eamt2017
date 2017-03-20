69f62a4cbce83c08ef9081268c1530aae09ad0b7
Jirka Balhar <balhar.j@gmail.com>
2017-03-20 07:06:11 +0100

    rozdělení na index a about

---
+++ about.php
@@ -0,0 +1,28 @@
+<?php
+$title = "About | EAMT 2017";
+include("header.html");
+?>
+<div class="column">
+	<header>
+	<h1>About the EAMT annual conference</h1>
+	<h2>EAMT 2017: The 20th Annual Conference of the European Association for Machine Translation</h2>
+	<p>Prague, Czech Republic
+	<br>May 29 to 31, 2017
+	<br><a href="https://ufal.mff.cuni.cz/eamt2017/">https://ufal.mff.cuni.cz/eamt2017/</a></p>
+	</header>
+
+	<p>The annual <a href="http://www.eamt.org" target="_blank">EAMT</a> conference showcases the very latest in MT tools and technology, with participants representing:</p>
+
+	<ul>
+		<li>companies and institutions already using or being potential users of MT</li>
+		<li>localisation companies using MT in the translation workflow</li>
+		<li>renowned MT scientists and researchers</li>
+		<li>companies and institutions developing MT systems</li>
+		<li>R&amp;D teams from both private companies and public universities</li>
+	</ul>
+
+	<p>The event regularly gathers around 150 participants from across Europe.</p>
+
+	<p>Conference presentations feature in-depth descriptions of the latest MT technology, enterprise use cases from large corporations and organisations using MT, best practices from the global localisation industry, the fruits of cutting-edge research from universities and research centers, and the results of EU-funded ICT projects.</p>
+</div>
+<?php include("footer.html"); ?>
--- index.php
+++ index.php
@@ -53,25 +53,6 @@ include("header.html");
 	<p>The European Association for Machine Translation (EAMT) invites everyone interested in machine translation, translation-related tools and resources to participate in this conference.
 	<br><a href="call-for-papers.php">[more]</a>
 	</p>
-</div>
-
-<div class="column">
-	<h2>About the EAMT annual conference</h2>
-
-	<p>The annual <a href="http://www.eamt.org" target="_blank">EAMT</a> conference showcases the very latest in MT tools and technology, with participants representing:</p>
-
-	<ul>
-		<li>companies and institutions already using or being potential users of MT</li>
-		<li>localisation companies using MT in the translation workflow</li>
-		<li>renowned MT scientists and researchers</li>
-		<li>companies and institutions developing MT systems</li>
-		<li>R&amp;D teams from both private companies and public universities</li>
-	</ul>
-
-	<p>The event regularly gathers around 150 participants from across Europe.</p>
-
-	<p>Conference presentations feature in-depth descriptions of the latest MT technology, enterprise use cases from large corporations and organisations using MT, best practices from the global localisation industry, the fruits of cutting-edge research from universities and research centers, and the results of EU-funded ICT projects.</p>
-
 	<h2>Organizers</h2>
 	<p>
 	Institute of Formal and Applied Linguistics <a href="http://ufal.mff.cuni.cz" target="_blank">(ÚFAL)</a><br>
