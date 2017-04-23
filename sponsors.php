4b4a68a2fa4b82241a5e80dd1d18a7ea46a73ece
Jirka Balhar <balhar.j@gmail.com>
2017-04-23 12:31:08 +0200

    Sponsors

---
+++ sponsors.php
@@ -0,0 +1,36 @@
+<?php
+$title = "Sponsors | EAMT 2017";
+include("header.html");
+?>
+<article>
+<div class="column">
+<header>
+<h1>Sponsors</h1>
+<h2>EAMT 2017: The 20th Annual Conference of the European Association for Machine Translation</h2>
+<p>Prague, Czech Republic
+<br>May 29&ndash;31, 2017
+<br><a href="https://ufal.mff.cuni.cz/eamt2017/">https://ufal.mff.cuni.cz/eamt2017/</a></p>
+</header>
+
+
+</div>
+
+<div class="column center">
+<section class="sponsors">
+<h3>Gold Sponsor</h3>
+<p>Memsource Translation and Localization Solutions</p>
+<a href="http://www.memsource.com/" title="Memsource">
+<img height="70" src="img/sponsors/memsource.png" alt="Memsource logo">
+</a>
+</section>
+<section class="sponsors">
+<h3>Bronze Sponsor</h3>
+<p>Charles University</p>
+<a href="http://www.cuni.cz/" title="Charles University" style="margin-top: 3em;">
+<img height="150" src="img/sponsors/uk.png" alt="Charles University logo">
+</a>
+</section>
+</div>
+
+</article>
+<?php include("footer.html"); ?>
