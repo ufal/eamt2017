ee6db88202476f60291831b12687e7bfe8b33bd2
Jirka Balhar <balhar.j@gmail.com>
2017-04-27 11:19:50 +0200

    Add venue

---
+++ venue.php
@@ -0,0 +1,246 @@
+<?php
+$title = "About | EAMT 2017";
+include("header.html");
+?>
+<div class="column">
+    <header>
+        <h1>Venue</h1>
+        <h2>EAMT 2017: The 20th Annual Conference of the European Association for Machine Translation</h2>
+        <p>Prague, Czech Republic
+            <br>May 29 to 31, 2017
+            <br><a href="https://ufal.mff.cuni.cz/eamt2017/">https://ufal.mff.cuni.cz/eamt2017/</a>
+        </p>
+    </header>
+
+    <h2>Annotated Google Map</h2>
+    <iframe src="https://www.google.com/maps/d/embed?mid=1GeFHE4Dn15il0ZLI4-nwVPoCvaw" width="640" height="480"></iframe>
+
+    <a name="building"></a>
+    <h2>Venue Address</h2>
+
+    <p>
+        Malostranské nám. 25
+        <br/>Praha 1
+        <br/>CZ-11800, Czech Republic
+    </p>
+
+    <h2>Faculty Building</h2>
+    <p>
+        The venue of MT Marathon 2016 (the Computer Science School building, see the left-hand picture below - on the horizon, the Prague Castle (Hradčany) can be seen as well) is in the center of Prague, in the Lesser Town. The building itself has been built in today's shape in the 17th century, and it served as a jesuit monastery. It has a rectangular shape with a small yard in the middle; three wings now belong to the University, and the fourth to the (roman-catholic) church, and in fact it is the largest baroque church in Prague (the right-hand picture).
+    </p>
+
+    <p>
+        <table>
+            <tr>
+                <td>
+                    <a href="img/mff1w.jpg"><img src="img/mff1t.jpg" alt="MFF UK building" />
+                    </a>
+                </td>
+                <td>
+                    <a href="img/mik1w.jpg"><img src="img/mik1t.jpg" alt="St. Nicholas Church" />
+                    </a>
+                </td>
+            </tr>
+        </table>
+    </p>
+
+    <ul>
+        <li>
+            <a href="http://360gigapixels.com/petrin-prague-photo/">36 Gigapixel 360&deg; view from Petřín tower.</a> Check how many of our department windows are open.
+        </li>
+        <li><a href="http://www.ms.mff.cuni.cz/tourist/msbuilding.html.en">Map of
+	floors</a>, indicating rooms and departments. We are ÚFAL, located on the fourth floor. Note that, typically of a CS department, the map is 1-indexed while the elevator and speakers of Czech use 0-based indexing.</li>
+    </ul>
+
+    <h2>Suggested Hotels and Hostels</h2>
+    <ul class="nodot_list">
+        <li>
+            <a href="http://www.biskupsky-dum.ubytovani-hotel-praha.eu/accommodation-Praha.htm">Hotel Biskupský Dům ****</a>
+        </li>
+        <li>
+            <a href="http://www.hotelutripstrosu.cz">Hotel U tří pštrosů ****</a>
+        </li>
+        <li>
+            <a href="http://www.kampagarden.cz">Hotel Kampa Garden ***</a>
+        </li>
+        <li>
+            <a href="http://malyglen.cz/en/show/accomodation">Hotel U maleho Glena</a>
+        </li>
+        <li>
+            <a href="http://www.trinidad.cz">Hotel Residence Trinidad ****</a>
+        </li>
+        <li>
+            <a href="http://www.certovka.cz">Hotel Čertovka ****</a>
+        </li>
+        <li>
+            <a href="http://www.hotel-roma-prague.com">Hotel Roma ****</a>
+        </li>
+        <li>
+            <a href="http://www.podvezi.com/">Hotel Pod Věží ****</a>
+        </li>
+    </ul>
+    <p>
+        Hostels:
+    </p>
+    <ul class="nodot_list">
+        <li>
+            <a href="http://www.prague-hostels.cz/">www.prague-hostels.cz</a>
+        </li>
+    </ul>
+    <p>
+        Participants can find suitable accommodation also on <a href="http://www.czechsite.com">www.czechsite.com</a> or <a href="http://www.abaka.com">www.abaka.com</a>
+    </p>
+
+
+    <h2>
+	Travelling to the Venue
+	</h2>
+
+    <p>
+        Prague can be easily reached from popular gateways to Europe including London, Paris, Amsterdam, Frankfurt, Zurich, Helsinki, Rome, Dubai etc. In addition, Prague can be reached by train or car from nearby cities such as Berlin, Dresden, Vienna and Munich.
+    </p>
+
+    <h3>
+	By Plane
+	</h3>
+
+    <p>
+        To get from the <a href="http://www.prg.aero/">Václav Havel Airport Prague (Ruzyně)</a> to the conference venue:
+    </p>
+
+    <h4>
+	Take an airport taxi
+	</h4>
+    <p>
+        We suggest to take ONLY AAA or 111 yellow taxis. Contact counters are located in arrival halls of both terminals.
+    </p>
+    <p>
+        The fare should not go over CZK 600 (USD 25). Taxi drivers should always issue a signed receipt. Ask the driver about the cost beforehand. They usually accept CCs (ask at the stand and the driver), CZK and EUR (not a very favorable exchange rate), but not USD.
+    </p>
+
+    <h4>
+	Use public transport
+	</h4>
+    <p>
+        The preferred route is the following:
+    </p>
+
+    <p>
+        <ol>
+            <li>
+                Take the bus no. 119 (direction Nádraží Veleslavín) to the terminal stop Nádraží Veleslavín.
+            </li>
+            <li>
+                Then take tram no. 20 (direction Sídliště Barrandov) and get off after 12 stops at Malostranské náměstí.
+            </li>
+        </ol>
+    </p>
+
+    <p>
+        The journey takes about 50 minutes in total, the fare is CZK 32 (ca. USD 1.30). You can view the journey on Google Maps along with other options (note that special, higher fare applies on the AE bus line and on Student Agency buses). More information about public transport in Prague can be found below.
+    </p>
+
+    <h3>
+	By Train or Bus
+	</h3>
+
+    <p>
+        Taking the train is a convenient option when travelling, e.g., from Berlin or Vienna. Night trains are also a good option esp. from more distant cities like Budapest or Warsaw. All long-distance trains arrive to the main railway station (Hlavní nádraží). Trains arriving from Berlin/Dresden stop at the Holešovice station, too. Most long-distance buses arrive at the Florenc bus station.
+    </p>
+
+    <p>
+        To get to the venue using public transport:
+    </p>
+
+    <h4>
+	From Hlavní nádraží (Main Station):
+	</h4>
+
+    <ol>
+        <li>
+            Take the metro line C (red line, direction Háje) to the Muzeum station (1 stop).
+        </li>
+        <li>
+            Then change to line A (green line, direction Dejvická) and get off at Malostranská (3 stops).
+        </li>
+        <li>
+            Then take tram no. 12, 20 or 22 (direction Sídliště Barrandov or Nádraží Hostivař) and get off at Malostranské náměstí (1 stop).
+        </li>
+    </ol>
+
+    <p>
+        The journey takes about 25 minutes, the fare is CZK 24 (ca. USD 1).
+    </p>
+
+    <h4>
+	From the Holešovice Railway Station:
+	</h4>
+
+    <ol>
+        <li>
+            take tram no. 12 (direction Sídliště Barrandov) and get off after 10 stops at Malostranské náměstí.
+        </li>
+    </ol>
+
+    <p>
+        The journey takes about 20 minutes, the fare is CZK 24 (ca. USD 1). You can view it on Google maps.
+    </p>
+
+    <h4>
+	From Florenc Bus Station:
+	</h4>
+
+    <ol>
+        <li>
+            Take the metro line B (yellow line, direction Zličín) to the Národní třída station (3 stops).
+        </li>
+        <li>
+            Take tram no. 22 (direction Bílá Hora) and get off at Malostranské náměstí (4 stops).
+        </li>
+    </ol>
+
+    <p>
+        The journey takes about 25 minutes, the fare is CZK 24 (ca. USD 1). You can view it on Google maps (along with other options).
+    </p>
+
+    <h4>
+	Using a Taxi
+	</h4>
+    <p>
+        We suggest to take AAA taxis only. Taxi stands are located near all stations (see details for Hlavní nádraží here). The fare should not be over CZK 250 (ask about it beforehand).
+    </p>
+
+    <h3>
+	To the venue by car
+	</h3>
+
+    <p>
+        A car is recommended to reach your hotel only. Prague city center has heavy traffic during rush hours and only a few available parking spots.
+    </p>
+
+    <h4>
+	Parking
+	</h4>
+
+    <p>
+        There is a paid car park on rear side of the building, limited to 2 hours of stay.
+    </p>
+    <p>
+        The nearest underground parking lot at Rudolfinum is about 15 min. walk away from the venue and has the same fee of CZK 60 / hour.
+    </p>
+
+
+    <h2>Other Useful Links</h2>
+
+    <ul>
+        <li><a href="http://www.prague.eu/en/">Prague Official Tourist Portal</a>.</li>
+        <li>Further on-line maps: <a href="http://www.mapy.cz/">Mapy.cz</a> include a full <a href="http://www.mapy.cz/zakladni?x=14.3988841&y=50.0913342&z=14&m3d=1">3D view of the city center</a>.</li>
+        <li><a href="http://www.dpp.cz/en/">Public transport</a>, including connection finder.</li>
+        <li><a href="http://jizdnirady.idnes.cz/vlakyautobusy/spojeni/">Journey planner for trains and buses</a> in the Czech Republic.</li>
+        <li>Printing shops (for printing posters):
+            <a href="http://www.xcopy.cz/kontakt/">Xcopy.cz</a>, <a href="http://www.copygeneral.eu/contact-us">Copy General</a>.
+        </li>
+    </ul>
+
+</div>
+<?php include("footer.html"); ?>
