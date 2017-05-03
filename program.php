67f1910a83afab3af295f355de0d405a9736cf16
Jirka Balhar <balhar.j@gmail.com>
2017-05-03 21:31:11 +0200

    add user track papers

--- program.php
+++ program.php
@@ -55,6 +55,21 @@ Everyone is invited to the annual General Assembly of the European Association f
     <li><span class="authors"><span><a href="http://www.ffzg.unizg.hr">Filip Klubička</a>, Antonio Toral and <a href="http://www.dlsi.ua.es/~vmsanchez/">Víctor M. Sánchez-Cartagena</a></span>. </span><span class="title">Fine-grained human evaluation of neural versus phrase-based machine translation</span></li>
     <li><span class="authors"><span>Hanna Bechara, Carla Parra Escartín and Constantin Orasan</span>. </span><span class="title">Questing for Quality Estimation</span></li>
     <li><span class="authors"><span>Ankit Srivastava, Georg Rehm and Felix Sasaki</span>. </span><span class="title">Improving Machine Translation through Linked Data</span></li>
-    </ul>
+</ul>
+<p><strong>Accepted User Track Papers</strong><br>
+<ul class="papers">
+    <li><span class="authors"><span>Nadira Hofmann and Maryse Lepan</span>. </span><span class="title">MT in real-world practice: Challenges and solutions at Swiss Federal Railways</span></li>
+    <li><span class="authors"><span>Anne Beyer, Vivien Macketanz, Aljoscha Burchardt and Philip Williams</span>. </span><span class="title">Can Out-of-the-box NMT Beat a Domain-trained Moses on Technical Data?</span></li>
+    <li><span class="authors"><span>Pierrette Bouillon, Johanna Gerlach, Hervé Spechbach, Nikos Tsourakis and Sonia Halimi</span>. </span><span class="title">BabelDr vs Google Translate: a user study at Geneva  University  Hospitals (HUG)</span></li>
+    <li><span class="authors"><span>Rei Miyata and Atsushi Fujita</span>. </span><span class="title">Dissecting Human Pre-Editing Toward Better Use of Off-the-Shelf Machine Translation Systems</span></li>
+    <li><span class="authors"><span>Joachim Van Den Bogaert, Bram Vandewalle and Roko Mijic</span>. </span><span class="title">Bootstrapping Quality Estimation  in a live production environment</span></li>
+    <li><span class="authors"><span>Tewodros Abebe and Michael Gasser</span>. </span><span class="title">A translation-based approach to the learning of the morphology of an under-resourced language</span></li>
+    <li><span class="authors"><span>Adrià Martín-Mor, Gökhan Doğru and Sergio Ortiz</span>. </span><span class="title">MTradumàtica: Free Statistical Machine Translation Customisation for Translators</span></li>
+    <li><span class="authors"><span>Lucia Comparin and Sara Mendes</span>. </span><span class="title">Using error annotation to evaluate machine translation and human post-editing in a business environment</span></li>
+    <li><span class="authors"><span>Pat Nagle, Riccardo Superbo, Laura Casanellas, Dimitar Shterionov and Tony O'Dowd</span>. </span><span class="title">Empirical evaluation of NMT and PBSMT quality for large-scale translation production.</span></li>
+    <li><span class="authors"><span>Pavel Levin, Nishikant Dhanuka and Maxim Khalilov</span>. </span><span class="title">Machine Translation at Booking.com: Journey and Lessons Learned</span></li>
+    <li><span class="authors"><span>Nicola Bertoldi, Roldano Cattoni, Mauro Cettolo, Mohammad Amin Farajian, Marcello Federico, Davide Caroselli, Luca Mastrostefano, Marco Trombetti, Ulrich Germann and David Madl</span>. </span><span class="title">AnonymizedSystemName: New Open Source MT for the Translation Industry</span></li>
+    <li><span class="authors"><span>Dimitra Kalantzi</span>. </span><span class="title">MT implementation for professional translators: Preliminary issues and challenges</span></li>
+</ul>
 </div>
 <?php include("footer.html"); ?>
