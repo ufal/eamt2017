d3b4597742b3b62610b3dc20f213ba5e80fdb9d2
Jirka Balhar <balhar.j@gmail.com>
2017-05-13 11:33:56 +0200

    publish program schedule

--- program-table.php
+++ program-table.php
@@ -1,9 +1,4 @@
 <?php
-$title = "Program | EAMT 2017";
-include("header.html");
-
-
-
 $document = "1JPlP7mlX7rb0uA5DtGe_J-B1Ge59dO8ZJze8cKuOsSc";
 $key = "AIzaSyDJOjkBsJe5A3VHPHtAW0N66xfhAcHivL8";
 $range = "Sheet1!A:B";
@@ -12,22 +7,6 @@ $program = file_get_contents("https://sheets.googleapis.com/v4/spreadsheets/$doc
 $sheetjson = json_decode($program);
 $table = $sheetjson->values;
 ?>
-
-<div class="column">
-<header>
-<h1>Program</h1>
-<h2>EAMT 2017: The 20th Annual Conference of the European Association for Machine Translation</h2>
-<p>Prague, Czech Republic
-<br>May 29 to 31, 2017
-<br><a href="https://ufal.mff.cuni.cz/eamt2017/">https://ufal.mff.cuni.cz/eamt2017/</a></p>
-<p>at the Faculty of Mathematics and Physics, Charles University,<br>
-Malostranské náměstí 25, 11800 Prague 1, Czech Republic<br>
-Room S9</p>
-
-</header>
-</div>
-
-<div class="column">
 <table class="program-table">
 	<?php
 	foreach ($table as $row) {
@@ -54,5 +33,3 @@ Room S9</p>
 	}
 	?>
 </table>
-</div>
-<?php include("footer.html"); ?>
--- program.php
+++ program.php
@@ -26,73 +26,7 @@ Adaptive Quality Estimation for Machine Translation and Automatic Speech Recogni
 
 <p><strong>EAMT General Assembly</strong><br>
 Everyone is invited to the annual General Assembly of the European Association for Machine Translation. But if you really want to take part in the future of the EAMT should, become a member: <a href="http://www.eamt.org/membership.php">http://www.eamt.org/membership.php</a>.</p>
-<p><strong>Accepted Research Papers</strong><br>
-<ul class="papers">
-    <li><span class="authors"><span><a href="http://www.lt3.ugent.be/">Arda Tezcan</a>, Veronique Hoste and <a href="http://www.lt3.ugent.be/">Lieve Macken</a></span>. </span><span class="title">A Neural Network Architecture for Detecting Grammatical Errors in Statistical Machine Translation</span></li>
-    <li><span class="authors"><span>Daniel Torregrosa, Juan Antonio Pérez-Ortiz and Mikel Forcada</span>. </span><span class="title">Comparative human and automatic evaluation of glass-box and black-box approaches to interactive translation prediction</span></li>
-    <li><span class="authors"><span><a href="http://researchmap.jp/reimiyata/">Rei Miyata</a>, <a href="http://english.rikkyo.ac.jp/">Anthony Hartley</a>, Kyo Kageura and <a href="http://www.ict.csiro.au/staff/Cecile.Paris">Cecile Paris</a></span>. </span><span class="title">Evaluating the Usability of a Controlled Language Authoring Assistant</span></li>
-    <li><span class="authors"><span>Anita Ramm, Riccardo Superbo, Dimitar Shterionov, Tony O'Dowd and Alexander Fraser</span>. </span><span class="title">Integration of a multilingual preordering component into a commercial SMT platform</span></li>
-    <li><span class="authors"><span>Aljoscha Burchardt, Vivien Macketanz, Jon Dehdari, Georg Heigold, Jan-Thorsten Peter and Philip Williams</span>. </span><span class="title">A Linguistic Evaluation of Rule-based, Phrase-based, and Neural MT Engines</span></li>
-    <li><span class="authors"><span>Jinhua Du and <a href="http://www.computing.dcu.ie/~away">Andy Way</a></span>. </span><span class="title">Pre-Reordering for Neural Machine Translation: Helpful or Harmful?</span></li>
-    <li><span class="authors"><span>Praveen Dakwale and Christof Monz</span>. </span><span class="title">Convolutional over Recurrent Encoder for Neural Machine Translation</span></li>
-    <li><span class="authors"><span><a href="http://http//:www.dlsi.ua.es/~mlf">Mikel L. Forcada</a>, <a href="http://http//:www.dlsi.ua.es/~fsanchez">Felipe Sánchez-Martínez</a>, <a href="http://http//:www.dlsi.ua.es/~mespla">Miquel Esplà-Gomis</a> and <a href="http://staffwww.dcs.shef.ac.uk/people/L.Specia/">Lucia Specia</a></span>. </span><span class="title">Towards optimizing MT for postediting effort: Can BLEU still be useful?</span></li>
-    <li><span class="authors"><span>Chiraag Lala, Pranava Madhyastha, Josiah Wang and Lucia Specia</span>. </span><span class="title">Unraveling the Contribution of Image Captioning and Neural Machine Translation for Multimodal Machine Translation</span></li>
-    <li><span class="authors">Maja Popovic. </span><span class="title">Comparing language related issues for NMT and PBMT between German and English</span></li>
-    <li><span class="authors"><span><a href="http://xixona.dlsi.ua.es/~fran/">Francis M. Tyers</a>, Hèctor Alòs I Font, Gianfranco Fronteddu and Adrià Martín-Mor</span>. </span><span class="title">Rule-based machine translation for the Italian--Sardinian language pair</span></li>
-    <li><span class="authors"><span>Marco Turchi, Matteo Negri, Amin Farajian and Marcello Federico</span>. </span><span class="title">Continuous Learning from Human Post-edits for Neural Machine Translation</span></li>
-    <li><span class="authors"><span><a href="https://www.limsi.fr/en/">Franck Burlot</a> and <a href="http://perso.limsi.fr/yvon">François Yvon</a></span>. </span><span class="title">Learning Morphological Normalization for Translation from and into Morphologically Rich Languages</span></li>
-    <li><span class="authors"><span>Sheila Castilho, Joss Moorkens, Federico Gaspari, Iacer Calixto, John Tinsley and Andy Way</span>. </span><span class="title">Is Neural Machine Translation the New State-of-the-Art?</span></li>
-    <li><span class="authors"><span><a href="http://www-i6.informatik.rwth-aachen.de/~peter">Jan-Thorsten Peter</a>, Arne Nix and <a href="http://www-i6.informatik.rwth-aachen.de/web/Staff/ney/">Hermann Ney</a></span>. </span><span class="title">Generating Alignments using Target Foresight in Attention-Based Neural Machine Translation</span></li>
-    <li><span class="authors"><span>Alberto Poncelas, Gideon Maillette de Buy Wenniger and <a href="http://www.computing.dcu.ie/~away">Andy Way</a></span>. </span><span class="title">Applying N-gram Alignment Entropy to Improve Feature Decay Algorithms</span></li>
-    <li><span class="authors"><span>Nasser Zalmout and Nizar Habash</span>. </span><span class="title">Optimizing Tokenization Choice for Machine Translation across Multiple Target Languages</span></li>
-    <li><span class="authors"><span>Peyman Passban, Qun Liu and Andy Way</span>. </span><span class="title">Providing Morphological Information for SMT Using Deep Neural Networks</span></li>
-    <li><span class="authors"><span>Pintu Lohar, Haithem Afli and Andy Way</span>. </span><span class="title">Maintaining sentiment polarity in translation of user-generated content</span></li>
-    <li><span class="authors"><span>Parnia Bahar, Tamer Alkhouli, Jan-Thorsten Peter, Christopher Jan-Steffen Brix and Hermann Ney</span>. </span><span class="title">Empirical Investigation of Optimization Algorithms in Neural Machine Translation</span></li>
-    <li><span class="authors"><span><a href="http://lvapeab.github.io/">Álvaro Peris</a>, Mara Chinea-Rios and <a href="http://www.iti.upv.es/~fcn/">Francisco Casacuberta</a></span>. </span><span class="title">Neural Networks Classifier for Data Selection in Statistical Machine Translation</span></li>
-    <li><span class="authors"><span>Miguel Domingo, Mara Chinea-Rios and Francisco Casacuberta</span>. </span><span class="title">Historical Documents Modernization</span></li>
-    <li><span class="authors">Eleftherios Avramidis. </span><span class="title">Comparative Quality Estimation for Machine Translation: Observations on machine learning and features</span></li>
-    <li><span class="authors">Vinit Ravishankar. </span><span class="title">Finite-state back-transliteration for Marathi</span></li>
-    <li><span class="authors"><span><a href="http://www.cs.upc.edu/~emartinez">Eva Martínez Garcia</a>, Cristina España-Bonet, Lluís Màrquez and Carles Creus</span>. </span><span class="title">Using Word Embeddings to Enforce Document-Level Lexical Consistency in Machine Translation</span></li>
-    <li><span class="authors"><span>Duygu Ataman, Matteo Negri, Marco Turchi and Marcello Federico</span>. </span><span class="title">Linguistically Motivated Vocabulary Reduction for Neural Machine Translation from Turkish to English</span></li>
-    <li><span class="authors"><span><a href="http://www.ffzg.unizg.hr">Filip Klubička</a>, Antonio Toral and <a href="http://www.dlsi.ua.es/~vmsanchez/">Víctor M. Sánchez-Cartagena</a></span>. </span><span class="title">Fine-grained human evaluation of neural versus phrase-based machine translation</span></li>
-    <li><span class="authors"><span>Carla Parra Escartín, Hanna Béchara and Constantin Orăsan</span>. </span><span class="title">Questing for Quality Estimation</span></li>
-    <li><span class="authors"><span>Ankit Srivastava, Georg Rehm and Felix Sasaki</span>. </span><span class="title">Improving Machine Translation through Linked Data</span></li>
-</ul>
-<p><strong>Accepted User Track Papers</strong><br>
-<ul class="papers">
-    <li><span class="authors"><span>Nadira Hofmann and Maryse Lepan</span>. </span><span class="title">MT in real-world practice: Challenges and solutions at Swiss Federal Railways</span></li>
-    <li><span class="authors"><span>Anne Beyer, Vivien Macketanz, Aljoscha Burchardt and Philip Williams</span>. </span><span class="title">Can Out-of-the-box NMT Beat a Domain-trained Moses on Technical Data?</span></li>
-    <li><span class="authors"><span>Pierrette Bouillon, Johanna Gerlach, Hervé Spechbach, Nikos Tsourakis and Sonia Halimi</span>. </span><span class="title">BabelDr vs Google Translate: a user study at Geneva  University  Hospitals (HUG)</span></li>
-    <li><span class="authors"><span>Rei Miyata and Atsushi Fujita</span>. </span><span class="title">Dissecting Human Pre-Editing Toward Better Use of Off-the-Shelf Machine Translation Systems</span></li>
-    <li><span class="authors"><span>Joachim Van Den Bogaert, Bram Vandewalle and Roko Mijic</span>. </span><span class="title">Bootstrapping Quality Estimation  in a live production environment</span></li>
-    <li><span class="authors"><span>Adrià Martín-Mor, Gökhan Doğru and Sergio Ortiz</span>. </span><span class="title">MTradumàtica: Free Statistical Machine Translation Customisation for Translators</span></li>
-    <li><span class="authors"><span>Lucia Comparin and Sara Mendes</span>. </span><span class="title">Using error annotation to evaluate machine translation and human post-editing in a business environment</span></li>
-    <li><span class="authors"><span>Pat Nagle, Riccardo Superbo, Laura Casanellas, Dimitar Shterionov and Tony O'Dowd</span>. </span><span class="title">Empirical evaluation of NMT and PBSMT quality for large-scale translation production.</span></li>
-    <li><span class="authors"><span>Pavel Levin, Nishikant Dhanuka and Maxim Khalilov</span>. </span><span class="title">Machine Translation at Booking.com: Journey and Lessons Learned</span></li>
-    <li><span class="authors"><span>Nicola Bertoldi, Roldano Cattoni, Mauro Cettolo, Mohammad Amin Farajian, Marcello Federico, Davide Caroselli, Luca Mastrostefano, Marco Trombetti, Ulrich Germann and David Madl</span>. </span><span class="title">AnonymizedSystemName: New Open Source MT for the Translation Industry</span></li>
-    <li><span class="authors"><span>Dimitra Kalantzi</span>. </span><span class="title">MT implementation for professional translators: Preliminary issues and challenges</span></li>
-</ul>
-
-<p><strong>Accepted Project and Product Papers</strong></p>
-<ul class="papers">
-    <li><span class="authors"><span>Pierrette Bouillon, Paula Estrella, Roxana Lafuente and Sabrina Girletti</span>. </span>MTTT – Machine Translation Training Tool: A tool to teach MT, Evaluation and Post-editing</li>
-    <li><span class="authors"><span>Vincent Vandeghinste, Tom Vanallemeersch, Liesbeth Augustinus, Frank Van Eynde, Joris Pelemans, Lyan Verwimp, Patrick Wambacq, Geert Heyman, Marie-Francine Moens, Iulianna van der Lek-Ciudin, Frieda Steurs, Ayla Rigouts Terryn, Els Lefever, Lieve Macken, Sven Coppers, Jan Van Den Bergh, Kris Luyten and Karin Coninx</span>. </span>SCATE - Smart Computer-Aided Translation Environment - Year 3</li>
-    <li><span class="authors"><span>Nadira Hofmann</span>. </span>TM &amp; MT – a happy couple […or how to calculate the potential benefit]</li>
-    <li><span class="authors"><span>Gary Evans, Alexander Ferrein and Winfried Kock</span>. </span>Towards Deploying CAT Tools in University Classesfor Improving Foreign Language Aquisition</li>
-    <li><span class="authors"><span>Josep Crego, Guillaume Klein, Jean Senellart, Yoon Kim, Yuntian Deng and Alexander M. Rush</span>. </span>OpenNMT: An Open-source Toolkit for Neural Machine Translation</li>
-    <li><span class="authors"><span>Alex Helle and Manuel Herranz</span>. </span>ActivaTM: A New Approach to Improve The Translation Memory Retrieval Process</li>
-    <li><span class="authors"><span>Celia Rico</span>. </span>IN-MIGRA2-CM. Why the Third Social Sector does Matter to MT</li>
-    <li><span class="authors"><span>Yu Gong and Demin Yan</span>. </span>A Tool Set to Integrate OpenNMT into Production Workflow</li>
-    <li><span class="authors"><span>Michael Gasser</span>. </span>Minimal Dependency Translation</li>
-    <li><span class="authors"><span>Rico Sennrich, Antonio Valerio Miceli Barone, Joss Moorkens, Sheila Castilho, Andy Way, Federico Gaspari, Valia Kordoni, Markus Egg, Maja Popovic, Yota Georgakopoulou, Maria Gialama and Menno van Zaanen</span>. </span>TraMOOC - Translation for Massive Open Online Courses: Recent Developments in Machine Translation</li>
-    <li><span class="authors"><span>Luchezar Jackov</span>. </span>SkyCode MT – a translation system using deep syntactic and semantic analysis</li>
-    <li><span class="authors">Ulrich Germann. </span>Progress in ModernMT, a New Open-Source Machine Translation Platform for the Translation Industry</li>
-    <li><span class="authors"><span>Julia Epiphantseva</span>. </span>PROMT Machine Translation for Amadeus Fare Quote Notes Translator</li>
-    <li><span class="authors"><span>Antonio Toral, Víctor Manuel Sánchez-Cartagena and Mikel Forcada</span>. </span>Final Results of Abu-MaTran (Automatic building of Machine Translation)</li>
-    <li><span class="authors"><span>Christian Federmann</span>. </span>Appraise on Azure: A cloud-based, multi-purpose evaluation framework</li>
-    <li><span class="authors"><span>Fatiha Sadat</span>. </span>Cross-lingual Social Media Analysis and Translation – The case of Arabic Tweets</li>
-    <li><span class="authors"><span>Barry Haddow, Alex Fraser, Marion Weller, Alexandra Birch, Ondrej Bojar, Fabienne Braune, Colin Davenport, Matthias Huck, Michal Kaspar, Kvetoslava Kovarikova, Josef Plch, Anita Ramm, Juliane Ried, James Sheary, Ales Tamchyna, Dusan Varis and Phil Williams</span>. </span>HimL : Health in my Language</li>
-    <li><span class="authors"><span>Tewodros Gebreselassie and Michael Gasser</span>. </span>A translation-based approach to the learning of the morphology of an under-resourced language</li>
-</ul>
+<p><strong>Schedule</strong><br>
+<?php include("program-table.php"); ?>
 </div>
 <?php include("footer.html"); ?>
